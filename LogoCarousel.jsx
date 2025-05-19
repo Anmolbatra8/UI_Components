9e5b75b6a5d727f987fdf6218178d4483152608f
Anmol Batra <115956152+Anmolbatra8@users.noreply.github.com>
2025-05-20 01:53:32 +0530

    Update LogoCarousel.jsx
    added sample data for rendering

--- LogoCarousel.jsx
+++ LogoCarousel.jsx
@@ -27,3 +27,29 @@ const LogoCarousel = ({ logoLinks, logoMainHeading }) => {
 };
 
 export default LogoCarousel;
+
+/*
+Sample Data For Test Rendering Locally Fast:
+You can copy paste below snippet to get started: 
+
+export const ClientLogos = [
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637680/Suzuki_SVG_zn9zhj.svg",
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637680/Rolls_Royce_SVG_cis5oc.svg",
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637680/Jeep_SVG_bipllq.svg",
+    
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637706/Mercedes_SVG_qnthrd.svg",
+   
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637681/Tata_SVG_z8j89w.svg",
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637680/KIA_SVG_lluume.svg",
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637678/Audi_SVG_lc5xhj.svg",
+    
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637678/BMW_Svg_duvqud.svg",
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637678/Fiat_SVG_ztqqja.svg",
+    "https://res.cloudinary.com/dyabjxxzp/image/upload/v1747637678/Ford_SVG_ix6jt5.svg",
+]
+
+     <LogoCarousel
+     logoMainHeading={"Modifying Accross Brands"}
+     logoLinks={ClientLogos}
+     />
+*/
